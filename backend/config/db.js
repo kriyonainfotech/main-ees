731bf1a57eb13d17a65cb32bdf833e945cebd279
kriyonainfotech <kriyonainfotech@gmail.com>
2024-12-26 06:37:25 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MongoDB)
+        const conn = await mongoose.connect('mongodb+srv://rajivmsurati11:rajivmsurati11@cluster0.p9hso.mongodb.net/EES-121_App')
         console.log(`MongoDB Connected: ${conn.connection.host}`)
     } catch (error) {
         console.log(error);
