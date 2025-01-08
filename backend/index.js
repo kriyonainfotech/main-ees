acbeeea3fda19acbb0c48ef06f4105c1eec7c0d3
prarthana-v <p.d.vaghani1729@gmail.com>
2025-01-08 18:39:37 +0530

    production-0.0

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,19 @@
-const dotenv = require('dotenv')
-const express = require('express');
+const dotenv = require("dotenv");
+const express = require("express");
 const app = express();
-const connectDB = require('./config/db');
+const connectDB = require("./config/db");
 const port = process.env.PORT || 3000;
-connectDB()
-dotenv.config()
+connectDB();
+dotenv.config();
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 
-// app.use(bodyParser.urlencoded({ extended: true })); 
+// app.use(bodyParser.urlencoded({ extended: true }));
 const cors = require("cors");
 const corsOptions = {
-    // origin: 'https://ess-frontend-eight.vercel.app', // Client URL
-    origin: 'http://localhost:5173', // Client URL
-    credentials: true, // Allow credentials (cookies, headers, etc.)
+  // origin: 'https://ess-frontend-eight.vercel.app', // Client URL
+  origin: "http://13.234.46.69:5173",
+  credentials: true, // Allow credentials (cookies, headers, etc.)
 };
 app.use(cors(corsOptions));
 app.use(express.json());
@@ -21,15 +21,15 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(express.urlencoded());
-app.get('/', (req, res) => {
-    res.send('Hello, world!');  // Root route to test if the server is up
+app.get("/", (req, res) => {
+  res.send("Hello, world!"); // Root route to test if the server is up
 });
-app.use('/', require('./routes/indexRoute'))
+app.use("/", require("./routes/indexRoute"));
 
 app.listen(port, (err) => {
-    if (err) {
-        console.log(err);
-        return false;
-    }
-    console.log(`Server is running on port ${port}`)
-})
+  if (err) {
+    console.log(err);
+    return false;
+  }
+  console.log(`Server is running on port ${port}`);
+});
